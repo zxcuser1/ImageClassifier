fd45be79930295e94ef7616d5d857ed932f2470c
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2025-05-28 15:01:43 +0300

    Update ImageSegmentor.py

--- ImageSegmentor.py
+++ ImageSegmentor.py
@@ -1,10 +1,12 @@
+import logging
+
 import cv2
 import numpy as np
 
 from BaseModel import BaseModel
 
 
-def extract_curves(predicted_mask, classes=range(1, 11), min_area=1500):
+def extract_curves(predicted_mask, classes=range(1, 11), min_area=1450):
     """
         Функция по выделению кривых в зависимости от класса
         :param: predicted_mask - предсказанная маска
@@ -37,23 +39,26 @@ class ImageSegmentor(BaseModel):
 
                :return: result - массив масок кривых с изображения
            """
-        prediction = self.model.predict(image)
+        try:
+            prediction = self.model.predict(image)
 
-        predicted_mask = np.argmax(prediction, axis=-1)[0]
+            predicted_mask = np.argmax(prediction, axis=-1)[0]
 
-        curve_dict = extract_curves(predicted_mask)
-        result = []
+            curve_dict = extract_curves(predicted_mask)
+            result = []
 
-        # Снимаем batch и возвращаем в uint8 формат
-        image_np = (image[0] * 255).astype(np.uint8)
+            # Снимаем batch и возвращаем в uint8 формат
+            image_np = (image[0] * 255).astype(np.uint8)
 
-        for cls, instances in curve_dict.items():
-            for idx, instance_mask in enumerate(instances):
-                # Проверим размеры на всякий случай
-                if image_np.shape[:2] != instance_mask.shape:
-                    instance_mask = cv2.resize(instance_mask, image_np.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+            for cls, instances in curve_dict.items():
+                for idx, instance_mask in enumerate(instances):
+                    # Проверим размеры на всякий случай
+                    if image_np.shape[:2] != instance_mask.shape:
+                        instance_mask = cv2.resize(instance_mask, image_np.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
 
-                masked = cv2.bitwise_and(image_np, image_np, mask=instance_mask)
-                result.append(masked)
+                    masked = cv2.bitwise_and(image_np, image_np, mask=instance_mask)
+                    result.append(masked)
 
-        return result
+            return result
+        except Exception as ex:
+            logging.error(f"Ошибка при сегментации изображения {ex}")
